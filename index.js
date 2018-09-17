16d1dcae292d09e3e27e87d698dd14dd8c14462c
Jakkrit Junrat <ball.vong.yo.gh@gmail.com>
2018-09-17 19:13:18 +0700

    emit new join

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
+
+  socket.on('new join', function(msg) {
+    io.emit('new join', msg);
+  });
 });
 
 http.listen(port, function(){
