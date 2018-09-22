4d15562561a4eb9bf42f886a793b43e6456d878f
Jakkrit Junrat <ball.vong.yo.gh@gmail.com>
2018-09-22 18:00:19 +0700

    local file db

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 var app = require('express')();
+var JsonDB = require('node-json-db');
+var db = new JsonDB("./db/myDataBase", true, false);
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+app.post('/:sender/:receiver', function(req, res) {
+  res.json(db.getData('/'+req.params.sender+'/'+req.params.receiver));
+});
+
 io.on('connection', function(socket){
   
-  socket.on('chat message', function(msg){
+  socket.on('chat message', function(msg) {
+    var json = JSON.parse(msg);
+    var from = json.from;
+    var to = json.to;
+    if (from && to) {
+      db.push('/'+from+'/'+to+'/chat[]', json);
+      db.push('/'+to+'/'+from+'/chat[]', json);
+    }
     io.emit('chat message', msg);
   });
 
